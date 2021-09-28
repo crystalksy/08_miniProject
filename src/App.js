d093fd245b1187641b6ac0c189a36d813cfb38e2
crystalksy <77429295+crystalksy@users.noreply.github.com>
2021-09-28 16:28:09 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './Pages/Home.jsx'
+import Contact from './Pages/Contact.jsx'
+import Review from './Pages/Review.jsx'
+import News from './Pages/News.jsx'
+
+
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/review" component={Review} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/news" component={News}/>
+      <Route />
+      {/* <News/> */}
+      
+    </Switch>
+  )
+}
+
+export default App
